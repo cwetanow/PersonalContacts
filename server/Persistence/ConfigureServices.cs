65d20a15f41988b8703ae2540cf64b7529c58065
Ivan Mladenov <mladenovv.ivan@gmail.com>
2023-03-10 20:06:52 +0200

    Configure db context

---
+++ server/Persistence/ConfigureServices.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence;
+public static class ConfigureServices
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, string connectionString) => services
+        .AddDbContext<PersonalContactsDbContext>(opts => opts.UseNpgsql(connectionString))
+        .AddScoped<DbContext, PersonalContactsDbContext>();
+}
