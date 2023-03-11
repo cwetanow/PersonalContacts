6b4590d2b5c36c5166cd26956ee710e617947223
Ivan Mladenov <mladenovv.ivan@gmail.com>
2023-03-11 11:20:09 +0200

    Update entities and value objects

---
+++ server/Domain/DateOfBirth.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Common;
+
+namespace Domain;
+public class DateOfBirth : ValueObject
+{
+    private DateOfBirth(int year, int month, int day)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+    }
+
+    public int Year { get; private set; }
+    public int Month { get; private set; }
+    public int Day { get; private set; }
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Year;
+        yield return Month;
+        yield return Day;
+    }
+
+    public static DateOfBirth Create(DateTime date) => new(date.Year, date.Month, date.Day);
+}
