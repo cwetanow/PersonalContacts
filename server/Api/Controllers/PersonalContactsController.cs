5a8227257ec32a6e8db51f5a2cf20aeea018dc1d
Ivan Mladenov <mladenovv.ivan@gmail.com>
2023-03-11 11:44:54 +0200

    Create endpoint

---
+++ server/Api/Controllers/PersonalContactsController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Commands;
+using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class PersonalContactsController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public PersonalContactsController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<Guid> AddContact(AddContact.Command request, CancellationToken cancellationToken)
+        => await mediator.Send(request, cancellationToken);
+}
