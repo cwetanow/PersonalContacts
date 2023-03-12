523b6d44d501075c47e89663b4b50d1141efd6ea
Ivan Mladenov <mladenovv.ivan@gmail.com>
2023-03-12 17:51:36 +0200

    Add resource for backend endpoints

--- client/src/app/personal-contacts/personal-contacts.resource.ts
+++ client/src/app/personal-contacts/personal-contacts.resource.ts
@@ -2,9 +2,13 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
 import { PersonalContactSimple } from './models/personal-contact-simple.model';
+import { PersonalContactDetail } from './models/personal-contact-details.model';
+import { ChangeContactRequest } from './requests/change-contact.request';
 
 @Injectable({ providedIn: 'root' })
 export class PersonalContactsResource {
+  private readonly url = '/api/v1/PersonalContacts';
+
   constructor(private http: HttpClient) { }
 
   getPersonalContacts(nameSearchTerm: string | null): Observable<PersonalContactSimple[]> {
@@ -13,10 +17,22 @@ export class PersonalContactsResource {
       params = params.set('nameSearchTerm', nameSearchTerm);
     }
 
-    return this.http.get<PersonalContactSimple[]>('/api/v1/PersonalContacts', { params });
+    return this.http.get<PersonalContactSimple[]>(this.url, { params });
+  }
+
+  getContactById(id: string): Observable<any> {
+    return this.http.get<PersonalContactDetail>(`${this.url}/${id}`);
+  }
+
+  addContact(request: ChangeContactRequest): Observable<string> {
+    return this.http.post<string>(this.url, request);
+  }
+
+  updateContact(id: string, request: ChangeContactRequest): Observable<string> {
+    return this.http.put<string>(`${this.url}/${id}`, request);
   }
 
   deleteContact(id: string): Observable<any> {
-    return this.http.delete(`/api/v1/PersonalContacts/${id}`);
+    return this.http.delete(`${this.url}/${id}`);
   }
 }
